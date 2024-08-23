6874bd1afca7cdda205dafcaf976a5ad4306223c
Kasun Nimantha <35260660+nimanthakasun@users.noreply.github.com>
2024-08-23 23:09:55 +0530

    First Commit

---
+++ best-practices/best-practices.cpp
@@ -0,0 +1,12 @@
+﻿// best-practices.cpp : Defines the entry point for the application.
+//
+
+#include "best-practices.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
